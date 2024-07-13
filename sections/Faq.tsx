699b5fba6b5d45b11e442a553923c2fe09533efb
@yuri_assuncx <killerkoll2012@gmail.com>
2024-07-13 04:42:54 -0300

    style (Faq): UI/UX improved

--- sections/Faq.tsx
+++ sections/Faq.tsx
@@ -1,3 +1,6 @@
+import type { RichText } from "apps/admin/widgets.ts";
+import Icon from "site/components/ui/Icon.tsx";
+
 export interface CTA {
   id?: string;
   href: string;
@@ -7,8 +10,7 @@ export interface CTA {
 
 export interface Question {
   title: string;
-  /** @format rich-text */
-  answer: string;
+  answer: RichText;
 }
 
 export interface Props {
@@ -19,10 +21,9 @@ export interface Props {
 }
 
 export default function BlogPosts({
-  title = "FAQs",
-  description =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
-  cta = { id: "change-me", href: "/", text: "Change me", outline: true },
+  title = "Looking for an answer?",
+  description,
+  cta,
   questions = [
     {
       title: "Question #1 text goes here",
@@ -52,46 +53,48 @@ export default function BlogPosts({
   ],
 }: Props) {
   return (
-    <div class="lg:container md:max-w-6xl lg:mx-auto mx-4 text-sm py-12 lg:py-28">
-      <div class="flex flex-col lg:flex-row gap-10 lg:gap-20 justify-between">
-        <div class="flex-none space-y-6 lg:w-2/5">
-          <p class="text-4xl leading-snug">
-            {title}
-          </p>
-          <p class="text-lg">
-            {description}
-          </p>
-          <a
-            key={cta?.id}
-            id={cta?.id}
-            href={cta?.href}
-            target={cta?.href.includes("http") ? "_blank" : "_self"}
-            class={`font-normal btn btn-primary ${
-              cta.outline && "btn-outline"
-            }`}
-          >
-            {cta?.text}
-          </a>
+    <div class="md:container md:max-w-4xl text-sm my-12 p-12 mx-4 md:mx-auto bg-secondary text-neutral rounded-3xl">
+      <div class="flex flex-col items-center justify-center gap-10 w-full">
+        <div class="flex-none space-y-6">
+          {title && (
+            <p class="text-4xl leading-snug font-black text-center">
+              {title}
+            </p>
+          )}
+
+          {description && (
+            <p class="text-lg">
+              {description}
+            </p>
+          )}
+
+          {cta && (
+            <a
+              key={cta.id}
+              id={cta.id}
+              href={cta.href}
+              target={cta.href.includes("http") ? "_blank" : "_self"}
+              class={`font-normal btn btn-primary ${
+                cta.outline && "btn-outline"
+              }`}
+            >
+              {cta.text}
+            </a>
+          )}
         </div>
-        <div class="flex-auto border-primary border-t">
+
+        <div class="flex-auto w-full">
           {questions?.map((question) => (
-            <details class="border-primary border-b group">
-              <summary class="text-lg cursor-pointer py-6 flex ">
-                <span class="flex-auto">{question.title}</span>
+            <details class="border-secondary-content border-b group">
+              <summary class="text-lg cursor-pointer py-6 flex">
+                <span class="font-bold flex-auto">{question.title}</span>
                 <span class="flex-none transition group-open:rotate-180">
-                  <svg
-                    width="32"
-                    height="33"
-                    viewBox="0 0 32 33"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M8.17674 12.5577L8.17676 12.5577L8.5303 12.2041C8.53031 12.2041 8.53031 12.2041 8.53032 12.2041C8.62794 12.1065 8.78621 12.1065 8.88385 12.2041C8.88385 12.2041 8.88385 12.2041 8.88385 12.2041L15.6464 18.9667L16 19.3202L16.3535 18.9667L23.1161 12.2041C23.2138 12.1064 23.372 12.1064 23.4696 12.2041L23.8232 12.5577C23.9208 12.6553 23.9208 12.8135 23.8232 12.9112L16.1767 20.5577C16.0791 20.6553 15.9209 20.6553 15.8232 20.5577L8.17674 12.9112C8.17674 12.9112 8.17674 12.9112 8.17674 12.9112C8.07911 12.8135 8.07911 12.6553 8.17674 12.5577Z"
-                      fill="#18181B"
-                      stroke="#18181B"
-                    />
-                  </svg>
+                  <Icon
+                    id="ChevronDown"
+                    size={32}
+                    strokeWidth={0.75}
+                    class="text-neutral"
+                  />
                 </span>
               </summary>
               <p
