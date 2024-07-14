f313b51b0fa6ecbac4253e713f40844599fe8ac8
@yuri_assuncx <killerkoll2012@gmail.com>
2024-07-14 10:52:26 -0300

    feat: added user to WelcomeSection

--- sections/WelcomeSection.tsx
+++ sections/WelcomeSection.tsx
@@ -1,22 +1,26 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
+import { TUser } from "site/loaders/user/get-by-email.ts";
+import { useUser } from "site/hooks/useUser.ts";
 
 export interface Props {
-  name: string;
+  user?: TUser | null;
   image: ImageWidget;
 }
 
-export default function WelcomeSection({ name, image }: Props) {
+export default function WelcomeSection({ user, image }: Props) {
+  if (!user) return null;
+
   return (
-    <section class="w-full max-w-6xl flex items-center justify-between container">
+    <section class="w-full max-w-6xl flex items-center justify-between container px-4 xl:px-0">
       <div class="flex flex-col">
         <p>Welcome back,</p>
-        <p class="text-3xl">Areetha Zoya</p>
+        <p class="text-3xl">{user.name}</p>
       </div>
       <div>
         <Image
           class="object-cover w-14 h-14 rounded-full"
-          alt={name}
+          alt={"Profile Image"}
           src={image}
           width={50}
           height={50}
@@ -25,3 +29,12 @@ export default function WelcomeSection({ name, image }: Props) {
     </section>
   );
 }
+
+export const loader = (props: Props, req: Request) => {
+  const user = useUser(null, req);
+
+  return {
+    ...props,
+    user,
+  };
+};
